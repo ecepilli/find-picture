adedc9f8503343c16c34a37eef68582696a022b9
ecepilli <ecepilli@hotmail.com>
2023-10-02 12:52:42 +0300

    api ile resimleri getirme

---
+++ app.js
@@ -0,0 +1,52 @@
+const formWrapper = document.querySelector(".form-wrapper");
+const form = document.querySelector("#form");
+const searchInput = document.querySelector("#searchInput");
+const buttonWrapper = document.querySelector(".button-wrapper");
+const searchButton = document.querySelector("#searchButton");
+const clearButton = document.querySelector("#clearButton");
+const imageListWrapper = document.querySelector(".imagelist-wrapper");
+
+runEventListeners();
+
+function runEventListeners() {
+    form.addEventListener("submit", search);
+}
+
+
+function search(e) {
+    const value = searchInput.value.trim();
+    // @RequestParam  - Spring- Rest APİ
+    fetch(`https://api.unsplash.com/search/photos?query=${value}`, {
+            method: "GET",
+            headers: {
+                Authorization: "Client-ID 0nWeGQx8u4GONMYQHvZB6QzLtoT8rc9MCJA28fsroMk"
+            }
+        })
+        .then((res) => res.json())
+        .then((data) =>
+            {
+                Array.from(data.results).forEach(image=> {
+                    // console.log(image.urls.small)
+                    addImageToUI(image.urls.small);
+                });
+            }
+        )
+
+        .catch((err) => console.log(err));
+
+
+    e.preventDefault();
+}
+
+function addImageToUI(url){
+    const div=document.createElement("div");
+    div.className="card";
+
+    const img= document.createElement("img");
+    img.setAttribute("src",url);
+    img.height="400";
+    img.width="400";
+
+    div.append(img);
+    imageListWrapper.append(div);
+}
